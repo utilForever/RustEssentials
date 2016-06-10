450675998a388612a06e96cda900ef59c3355542
Chris Ohk <utilForever@gmail.com>
2016-06-10 12:57:40 +0900

    Add Chapter 3\Codes\ExDoc project (execute rustdoc)

---
+++ Chapter 3/Codes/ExDoc/src/doc/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["main"] = {"doc":"","items":[[5,"cube","main","Calculates the cube `val * val * val`.",null,{"inputs":[{"name":"u32"}],"output":{"name":"u32"}}]],"paths":[]};
+initSearch(searchIndex);
